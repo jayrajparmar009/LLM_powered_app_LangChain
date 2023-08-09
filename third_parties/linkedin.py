d94c8c316ce9b7c24edcddd7022371522bbff402
Jayraj Parmar <jayrajparmar009@gmail.com>
2023-08-09 19:03:18 -0400

    Updated linkedin scrape code

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -6,4 +6,5 @@ import requests
 def scrape_linkedin_profile(linkedin_profile_url: str):
     """Scrape information from LinkedIn profiles
     Manually scrape the information from the linkedin profile"""
+    pass
 
