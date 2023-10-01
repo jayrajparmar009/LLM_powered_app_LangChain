429cc6228f82a4aa55516579a8f452e8013683b9
Jayraj Parmar <jayrajparmar009@gmail.com>
2023-09-30 22:37:21 -0400

    Added Output Parser for getting final result in object

--- ice_breaker.py
+++ ice_breaker.py
@@ -8,11 +8,10 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
+from Output_Parser import person_intel_parser, PersonIntel
 
-name = "Elon Musk"
-if __name__ == "__main__":
-    print("Hello LangChain!")
 
+def ice_breaker(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -20,24 +19,32 @@ if __name__ == "__main__":
     tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     summary_template = """
-        given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person, I want you to create:
-         1. a short summary
-         2. two interesting facts about them
-         3. A topic that may interest them
-         4. 2 creative Ice breakers to open a conversation with them 
+            given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person, I want you to create:
+             1. a short summary
+             2. two interesting facts about them
+             3. A topic that may interest them
+             4. 2 creative Ice breakers to open a conversation with them 
+                \n{format_instructions}
 
-        """
+            """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    print(linkedin_profile_url)
-    print(
-        chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
-    )
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    print(result)
+    return person_intel_parser.parse(result)
 
-    # print(scrape_user_tweets(username="SteveSaretsky", num_tweets=5))
+
+if __name__ == "__main__":
+    print("Hello LangChain!")
+
+name = "Elon Musk"
+ice_breaker(name=name)
